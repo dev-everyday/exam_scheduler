494ac40258a7c8b05a852d8fb0f3b48257db2899
dev-everyday <everyharus@gmail.com>
2025-04-09 19:27:58 +0900

    chore: add django_redis config

--- exam_scheduler/settings.py
+++ exam_scheduler/settings.py
@@ -123,3 +123,19 @@ AUTH_USER_MODEL = 'users.User'
 
 # Login URL
 LOGIN_URL = '/users/login/'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+REDIS_LOCK_TIMEOUT = 30
+REDIS_LOCK_BLOCKING_TIMEOUT = 10
