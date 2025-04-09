b8f6b6f043faf576b07f328af0f2dafc58b23e21
dev-everyday <everyharus@gmail.com>
2025-04-09 17:41:21 +0900

    chore: modify LoginSerializer from ModelSerializer to Serializer - name duplicate check when login because of ModelSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -25,11 +25,6 @@ class UserUpdateSerializer(serializers.ModelSerializer):
             'password': {'required': False, 'help_text': '새로운 비밀번호 (선택)', 'write_only': True}
         }
 
-class LoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
-        extra_kwargs = {
-            'username': {'help_text': '사용자명 (필수)'},
-            'password': {'help_text': '비밀번호 (필수)', 'write_only': True}
-        }        
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(help_text='사용자명 (필수)')
+    password = serializers.CharField(help_text='비밀번호 (필수)', write_only=True)        
