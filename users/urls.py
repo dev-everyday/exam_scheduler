63ce1d8cd153ee43e7f422bbb6a63c981582fa55
dev-everyday <everyharus@gmail.com>
2025-04-10 12:35:36 +0900

    chore: modify urlpatterns to make clear action

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', views.user_view, name='users'),
+    path('signup/', views.user_view, name='users'),
     path('my/', views.user_detail_view, name='user_detail'),
     path('admin/', views.admin_user_view, name='user_admin'),
     path('admin/<int:user_id>/', views.admin_user_detail_view, name='user_admin_detail'),
