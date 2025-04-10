0ea0d7b3d94aa159657811a8fa8a61961b0a9181
dev-everyday <everyharus@gmail.com>
2025-04-10 14:53:47 +0900

    chore: make ErrorResponseSerializer more flexible

--- common/serializers.py
+++ common/serializers.py
@@ -1,4 +1,22 @@
 from rest_framework import serializers
 
 class ErrorResponseSerializer(serializers.Serializer):
-    error = serializers.CharField(help_text='에러 메시지') 
+    """
+    에러 응답을 위한 serializer
+    - 단일 에러 메시지: {'error': '에러 메시지'}
+    - 필드별 에러: {'errors': {'필드1': ['에러1', '에러2'], '필드2': ['에러1']}}
+    """
+    error = serializers.CharField(help_text='에러 메시지', required=False)
+    errors = serializers.DictField(help_text='필드별 에러 메시지', required=False)
+    
+    def to_representation(self, instance):
+        if isinstance(instance, dict) and ('error' in instance or 'errors' in instance):
+            return super().to_representation(instance)
+            
+        if isinstance(instance, dict) and not ('error' in instance or 'errors' in instance):
+            return {'errors': instance}
+            
+        if isinstance(instance, str):
+            return {'error': instance}
+            
+        return {'error': str(instance)} 
